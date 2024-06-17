d3306c9c1f6fd30e971afc4a5fcb3ceb8dc97f34
Younghun Kang <younghun@utexas.edu>
2024-06-16 22:20:56 -0500

    Change error messages
    Former-commit-id: a88beba370e6a21da08fe7cf0555519c66bd6647

--- libraries/admesh library/CheckElevationDataFile.m
+++ libraries/admesh library/CheckElevationDataFile.m
@@ -1,12 +1,22 @@
 function CheckElevationDataFile(app)
 
 filename = app.ElevationDataFilename;
-if isempty(filename) || exist(filename,'file') == 0
-    msg = 'No elevation data is loaded.';
+if isempty(filename)
+    msg = 'No elevation data is specified.';
     choice = uiconfirm(app.UIFigure,msg,'ADMESH',...
         'Options',{'OK'},'DefaultOption',1,'Icon','Error');
     % Without this if statement, the subapp is closed immediately
     if strcmp(choice,'OK')
         delete(app);
     end
+
+elseif ~isempty(filename) && exist(filename,'file') == 0
+    msg = ['Specified elevation data "',filename, '" cannot be loaded.'];
+    choice = uiconfirm(app.UIFigure,msg,'ADMESH',...
+        'Options',{'OK'},'DefaultOption',1,'Icon','Error');
+    % Without this if statement, the subapp is closed immediately
+    if strcmp(choice,'OK')
+        delete(app);
+    end
+
 end
